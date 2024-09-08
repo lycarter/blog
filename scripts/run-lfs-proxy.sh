596100bb58d6f6d46f5f4eb5792c4929a04a2d97
Landon Carter <lcarter1239@gmail.com>
2024-09-07 23:37:00 -0400

    make script a bit smarter

--- scripts/run-lfs-proxy.sh
+++ scripts/run-lfs-proxy.sh
@@ -2,11 +2,10 @@
 
 echo "Starting LFS proxy server"
 
-# echo first char of $AWS_ACCESS_KEY_ID
-echo ${AWS_ACCESS_KEY_ID:0:1}
-
-# AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)
-# AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
+if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)
+AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
+fi
 
 echo "calling npx"
 
