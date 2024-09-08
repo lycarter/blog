a25be3937146678af0b0eeb53f83f6da81f6b194
Landon Carter <lcarter1239@gmail.com>
2024-09-07 22:53:31 -0400

    sleep 20

--- scripts/clone-lfs.sh
+++ scripts/clone-lfs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-(echo "going to sleep"; sleep 5; echo "awake"; git lfs pull; echo "done"; kill $$) &
+(echo "going to sleep"; sleep 20; echo "awake"; git lfs pull; echo "done"; kill $$) &
 
 # run server in the background and direct output to stdout
 bash scripts/run-lfs-proxy.sh
