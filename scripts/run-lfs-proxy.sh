92921f87d98a7f069a87f8185c668a0dfe14e1c2
Landon Carter <lcarter1239@gmail.com>
2024-09-07 23:00:08 -0400

    tweak

--- scripts/run-lfs-proxy.sh
+++ scripts/run-lfs-proxy.sh
@@ -2,8 +2,11 @@
 
 echo "Starting LFS proxy server"
 
-AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)
-AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
+# echo first char of $AWS_ACCESS_KEY_ID
+echo ${AWS_ACCESS_KEY_ID:0:1}
+
+# AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)
+# AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
 
 echo "calling npx"
 
