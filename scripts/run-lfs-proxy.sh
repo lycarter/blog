45d558ec8ff2ac775870eb2458c3c5a2eff67cae
Landon Carter <lcarter1239@gmail.com>
2024-09-07 22:54:53 -0400

    sleep 40

--- scripts/run-lfs-proxy.sh
+++ scripts/run-lfs-proxy.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+echo "Starting LFS proxy server"
+
 AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)
 AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
 
+echo "calling npx"
+
 npx --yes "github:lycarter/node-git-lfs"
+
+echo "done with lfs proxy"
