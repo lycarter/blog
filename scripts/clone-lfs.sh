cbf9f7c7e6d72d781acddfd31e97f0c83eaea697
Landon Carter <lcarter1239@gmail.com>
2024-09-08 00:33:36 -0400

    reverse who runs in the foreground in script

--- scripts/clone-lfs.sh
+++ scripts/clone-lfs.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-(echo "going to sleep"; sleep 40; echo "awake"; git lfs pull; echo "done"; kill $$) &
 
 # run server in the background and direct output to stdout
-bash scripts/run-lfs-proxy.sh
+bash scripts/run-lfs-proxy.sh &
+
+echo "going to sleep"
+sleep 25
+echo "awake"
+git lfs pull
+echo "done"
+exit 0
